506f567b6538edfb11af842fe3dc22815654ce52
Raphael Augusto <raphaelaugustoteixeirasilva@gmail.com>
2019-12-29 19:22:17 -0300

    Updated the "license" description to be more complete

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
     'Development Status :: 3 - Alpha',      
     'Intended Audience :: Developers',      
     'Topic :: Software Development :: Build Tools',
-    'License :: OSI Approved :: GPL',   
+    'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',   
     'Programming Language :: Python :: 2.7',
     'Programming Language :: Python :: 3.6',
   ],
