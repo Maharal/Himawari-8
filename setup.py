1e4dd7ef6aa49891543b1299dcf3dadf960a68dd
Raphael Augusto <raphaelaugustoteixeirasilva@gmail.com>
2019-12-29 20:17:40 -0300

    fix some errors

--- setup.py
+++ setup.py
@@ -1,19 +1,21 @@
 from distutils.core import setup
 setup(
   name = 'himawari8',         
-  version = '0.6',      
+  version = '0.7',      
   packages = ['himawari8'],  
   license='GPL-3.0',        
   description = 'Assists in downloading images of planet earth from the japanese satellite himawari 8',   
   author = 'Raphael Augusto Teixeira Silva',                 
   author_email = 'raphaelaugustoteixeirasilva@gmail.com',      
   url = 'https://github.com/Maharal/Himawari-8',  
-  download_url = 'https://github.com/Maharal/Himawari-8/archive/v0.6.tar.gz', 
+  download_url = 'https://github.com/Maharal/Himawari-8/archive/v0.7.tar.gz', 
   keywords = ['earth', 'himawari 8', 'planet', 'photo'],   
   install_requires=[           
           'requests',
           'Pillow',
           'tqdm',
+          'setuptools',
+          'wheel'
       ],
   classifiers=[
     'Development Status :: 3 - Alpha',      
